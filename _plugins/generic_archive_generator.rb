0dbd60d9c44d3857cab5ee723fddb8aae613e3ca
Michael Bikovitsky <moshe.biko@gmail.com>
2014-12-30 11:54:09 +0200

    Prettify documentation

--- _plugins/generic_archive_generator.rb
+++ _plugins/generic_archive_generator.rb
@@ -5,14 +5,14 @@ module Jekyll
 
     # Initializes a new ArchivePage instance.
     #
-    # @param site [Site] the Jekyll site instance.
-    # @param base [String] the path to the source.
-    # @param dir [String] the path between the source and the file.
-    # @param name [String] the filename of the file.
-    # @param template_path [String] path to the layout template to use.
-    # @param archive_id [String] an identifier for the archive being generated.
-    # @param page_id [String] an identifier for the current archive page.
-    # @param posts [Array<Post>] the posts to include in the page.
+    # @param site          [Site]        the Jekyll site instance.
+    # @param base          [String]      the path to the source.
+    # @param dir           [String]      the path between the source and the file.
+    # @param name          [String]      the filename of the file.
+    # @param template_path [String]      path to the layout template to use.
+    # @param archive_id    [String]      an identifier for the archive being generated.
+    # @param page_id       [String]      an identifier for the current archive page.
+    # @param posts         [Array<Post>] the posts to include in the page.
     def initialize(site, base, dir, name, template_path, archive_id, page_id, posts)
       @site = site
       @base = base
@@ -37,11 +37,11 @@ module Jekyll
 
     # Initializes a new GenericArchiveGenerator instance.
     #
-    # @param site [Site] the Jekyll site instance.
-    # @param archive_id [String] an identifier for the archive being generated.
+    # @param site          [Site]                        the Jekyll site instance.
+    # @param archive_id    [String]                      an identifier for the archive being generated.
     # @param archive_posts [Hash{String => Array<Post>}] the posts to generate the archive from.
-    # @param base_dir [String] a path relative to the source where the archive should be placed.
-    # @param template_path [String] path to the layout template to use.
+    # @param base_dir      [String]                      a path relative to the source where the archive should be placed.
+    # @param template_path [String]                      path to the layout template to use.
     def initialize(site, archive_id, archive_posts, base_dir, template_path)
       @site          = site
       @archive_id    = archive_id
