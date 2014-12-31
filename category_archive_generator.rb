22869983ff10b8e025c50765145e112c4dcc17ec
Michael Bikovitsky <moshe.biko@gmail.com>
2014-12-31 17:51:45 +0200

    Add category_links filter

--- category_archive_generator.rb
+++ category_archive_generator.rb
@@ -30,4 +30,36 @@ module Jekyll
 
   end
 
+  module Filters
+
+    # Outputs a list of categories as comma-separated <a> links. This is used
+    # to output the category list for each post on a category page.
+    #
+    # This is adapted from +generate_categories.rb+
+    # (https://github.com/recurser/jekyll-plugins).
+    #
+    # @param categories [Array<String>] the list of categories to format.
+    #
+    # @return [String]
+    def category_links(categories)
+      base_dir = @context.registers[:site].config["category_archive_base"]
+      categories = categories.sort!.map do |category|
+        category_dir = ArchivePage.archive_dir(base_dir, category)
+        # Make sure the category directory begins with a slash.
+        category_dir = "/#{category_dir}" unless category_dir =~ /^\//
+        "<a class='category' href='#{category_dir}/'>#{category}</a>"
+      end
+
+      case categories.length
+      when 0
+        ""
+      when 1
+        categories[0].to_s
+      else
+        categories.join(", ")
+      end
+    end
+
+  end
+
 end
